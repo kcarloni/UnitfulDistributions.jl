0da434b1859c5b47867a358d52a619b04adab4bc
kcarloni <kcarloni@g.harvard.edu>
2025-06-13 18:57:46 -0400

    add convenience multiply

--- src/UnitfulDistributions.jl
+++ src/UnitfulDistributions.jl
@@ -1,6 +1,7 @@
 module UnitfulDistributions
 
 using Unitful: Quantity, oneunit
+using Distributions
 using Distributions: UnivariateDistribution
 import Distributions: pdf 
 
@@ -12,7 +13,7 @@ import Distributions: pdf
         args = promote( args... )
         T = typeof(args[1])
         UnitfulDistribution{UD, T}(
-            UD( (args ./ oneunit(T))... )
+            UD( (args .÷ oneunit(T))... )
         )
     end
 
@@ -24,13 +25,23 @@ import Distributions: pdf
         end
     end
 
-    function pdf( UD::UnitfulDistribution{D, Q}, x::Q ) where {D, Q}
+    Distributions.StatsAPI.params( UD::UnitfulDistribution ) = params( UD.D )
+
+    # ----------------------
+
+    function pdf( UD::UnitfulDistribution{D, Q}, x::AbstractArray{Q,M} ) where {D, Q, M}
         return pdf( UD.D, x/oneunit(Q) ) / oneunit(Q)
     end
 
+    # ----------------------
+
+    # convenience constructor: allows you to write `Normal( 1s, 10s )`
     function (D::Type{T})( args::Vararg{Q,N} ) where {T<:UnivariateDistribution, Q<:Quantity, N}
         UnitfulDistribution{D}(args...)
     end
+
+    # convenience constructor: allows you to write `Normal() * 1s`
+    Base.:*( d::D, x::Quantity ) where {D <: Distribution} = UnitfulDistribution{D}( (params(d) .* x)... )
 
     export UnitfulDistribution
 
